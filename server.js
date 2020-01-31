70418e458a0eb001f1869977d3521d518cda162b
Eliana Rodriguez <erodriguez@primary.com.ar>
2020-01-31 16:31:07 -0300

    change server js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/reddis-like-test'));
+app.use(express.static(__dirname + '/dist/reddit-like-test'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile('./dist/reddis-like-test/index.html');
+res.sendFile(path.join(__dirname+'/dist/reddit-like-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
